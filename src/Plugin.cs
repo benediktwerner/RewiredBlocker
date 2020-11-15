9a3c533e339e627868612bf80e72290aac9047da
Benedikt Werner <1benediktwerner@gmail.com>
2020-11-15 10:04:29 +0100

    Version 1.0

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,13 +1,54 @@
 ﻿using BepInEx;
+using System;
+using System.Linq;
+using UnityEngine;
 
 namespace RewiredIMGUIBlocker
 {
     [BepInPlugin(PluginInfo.PLUGIN_ID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        private void Awake()
+        const string REWIRED_ASSEMBLY_NAME = "Rewired_Core";
+
+        static bool hasRewired = false;
+        static bool shouldReenableKeyboard = false;
+
+        void Awake()
         {
-            // Insert code that runs once the plugin starts
+            var rewiredAssembly = AppDomain.CurrentDomain
+                .GetAssemblies()
+                .SingleOrDefault(assembly => assembly.GetName().Name.Equals(REWIRED_ASSEMBLY_NAME, StringComparison.OrdinalIgnoreCase));
+
+            hasRewired = rewiredAssembly != null;
+
+            if (hasRewired) Logger.LogInfo("Rewired found.");
+            else Logger.LogWarning("Rewired NOT found. Going to sleep.");
+        }
+
+        void OnGUI()
+        {
+            if (!hasRewired) return;
+
+            PerformBlock();
+        }
+
+        // Leave this in a separate method to avoid problems if Rewired is not loaded
+        static void PerformBlock()
+        {
+            var canBeEnabled = GUIUtility.keyboardControl == 0;
+            var keyboard = Rewired.ReInput.controllers.Keyboard;
+
+            if (keyboard.enabled == canBeEnabled) return;
+            if (keyboard.enabled)
+            {
+                keyboard.enabled = false;
+                shouldReenableKeyboard = true;
+            }
+            else if (shouldReenableKeyboard)
+            {
+                keyboard.enabled = true;
+                shouldReenableKeyboard = false;
+            }
         }
     }
 }
