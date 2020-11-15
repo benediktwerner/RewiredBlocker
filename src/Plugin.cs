c47212f0b7abc7f212073a2e9fa2490598070c05
Benedikt Werner <1benediktwerner@gmail.com>
2020-11-15 08:03:17 +0100

    Initial commit

---
+++ src/Plugin.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace RewiredIMGUIBlocker
+{
+    [BepInPlugin(PluginInfo.PLUGIN_ID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Insert code that runs once the plugin starts
+        }
+    }
+}
